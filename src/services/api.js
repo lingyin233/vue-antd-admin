1adb149299b4ec05902c1059a8cc437c0ec381a0
zhongzt <zhongzt@foxmail.com>
2023-11-22 15:07:30 +0800

    update visit path

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 //跨域代理前缀
 const API_PROXY_PREFIX='/obexxadmin';
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX;
+console.log("BASE_URL:", BASE_URL);
 // const BASE_URL = process.env.VUE_APP_API_BASE_URL
 console.log('base_url=', BASE_URL);
 module.exports = {
