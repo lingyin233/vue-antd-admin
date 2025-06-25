b321006e56fe03ad0b730a818d0156fc6a43a234
luxirui <824939892@qq.com>
2025-06-25 17:43:53 +0800

    删除功能

--- src/services/api.js
+++ src/services/api.js
@@ -73,4 +73,5 @@ module.exports = {
   OTHER_DEVICE_ADD: `${BASE_URL}/other/device/add`,
   OTHER_DEVICE_LIST: `${BASE_URL}/other/device/list`,
   OTHER_DEVICE_UPDATE_STATUS: `${BASE_URL}/other/device/updateStatus`,
+  OTHERUPDATE_DEL:`${BASE_URL}//other/device/del`
 };
--- src/services/other.js
+++ src/services/other.js
@@ -1,5 +1,5 @@
 import { request, METHOD } from '@/utils/request';
-import {OTHER_DEVICE_ADD,OTHER_DEVICE_LIST,OTHER_DEVICE_UPDATE_STATUS } from './api';
+import {OTHER_DEVICE_ADD,OTHER_DEVICE_LIST,OTHER_DEVICE_UPDATE_STATUS,OTHERUPDATE_DEL } from './api';
 export function otherDeviceAdd(param) {
   return request(OTHER_DEVICE_ADD, METHOD.POST, param);
 }
@@ -9,9 +9,13 @@ export function otherDeviceList(param) {
 export function otherDeviceUpdateStatus(param) {
   return request(OTHER_DEVICE_UPDATE_STATUS, METHOD.GET, param);
 }
+export function delOtherUpdate(param) {
+  return request(OTHERUPDATE_DEL, METHOD.GET, param);
+}
 export default {
   otherDeviceAdd,
   otherDeviceList,
   otherDeviceUpdateStatus,
+  delOtherUpdate,
 };
   
